686e8838fbcbd6e0813dbf3eb83b90570842e478
stevenescape-tech <stevenescape@gmail.com>
2025-10-21 18:22:46 -0400

    Initial commit from your app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { FullWidth } from "./screens/FullWidth";
 
 export const App = () => {
-  return <FullWidth />;
+  return (
+    <>
+      <FullWidth />
+      <Analytics />
+    </>
+  );
 };
